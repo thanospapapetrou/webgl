e8c0d87de29c608f75fe89d7ef5614c6445eb553
Athanasios Papapetrou <athanasios.papapetrou@netcompany.com>
2025-04-05 20:28:04 +0300

    Animation

--- WebGL.js
+++ WebGL.js
@@ -22,15 +22,24 @@ class WebGL {
     `;
 
     #gl;
+    #time;
+    #rotation;
+    #dt;
+    #programInfo;
+    #buffers;
 
     static main() {
-        new WebGL(document.querySelector('canvas#gl').getContext('webgl'));
+        const webGl = new WebGL(document.querySelector('canvas#gl').getContext('webgl'));
+        requestAnimationFrame(webGl.render.bind(webGl));
     }
 
     constructor(gl) {
         this.#gl = gl;
+        this.#time = 0;
+        this.#rotation = 0;
+        this.#dt = 0;
         const shaderProgram = this.initShaderProgram();
-        const programInfo = {
+        this.#programInfo = {
             program: shaderProgram,
             attribLocations: {
                 vertexPosition: this.#gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
@@ -41,8 +50,8 @@ class WebGL {
                 modelViewMatrix: this.#gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
             }
         };
-        const buffers = this.initBuffers();
-        this.drawScene(programInfo, buffers);
+        this.#buffers = this.initBuffers();
+        this.drawScene();
     }
 
     initShaderProgram() {
@@ -88,7 +97,7 @@ class WebGL {
       return positionBuffer;
     }
 
-    drawScene(programInfo, buffers) {
+    drawScene() {
         this.#gl.clearColor(0.0, 0.0, 0.0, 1.0); // black
         this.#gl.clearDepth(1.0);
         this.#gl.enable(this.#gl.DEPTH_TEST);
@@ -101,36 +110,37 @@ class WebGL {
         const projectionMatrix = mat4.create();
         mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
         const modelViewMatrix = mat4.create();
-        mat4.translate(modelViewMatrix, // destination matrix
-        modelViewMatrix, // matrix to translate
-        [-0.0, 0.0, -6.0]); // amount to translate
-        this.setPositionAttribute(buffers, programInfo);
-        this.setColorAttribute(buffers, programInfo);
-        this.#gl.useProgram(programInfo.program);
-        this.#gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
-        this.#gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+        mat4.translate(modelViewMatrix, modelViewMatrix, // matrix to translate
+                [-0.0, 0.0, -6.0]); // amount to translate
+        mat4.rotate(modelViewMatrix, modelViewMatrix,
+                this.#rotation, [0, 0, 1]); // axis to rotate around
+        this.setPositionAttribute();
+        this.setColorAttribute();
+        this.#gl.useProgram(this.#programInfo.program);
+        this.#gl.uniformMatrix4fv(this.#programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
+        this.#gl.uniformMatrix4fv(this.#programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
         const offset = 0;
         const vertexCount = 4;
         this.#gl.drawArrays(this.#gl.TRIANGLE_STRIP, offset, vertexCount);
     }
 
-    setPositionAttribute(buffers, programInfo) {
+    setPositionAttribute() {
         const numComponents = 2; // pull out 2 values per iteration
         const type = this.#gl.FLOAT; // the data in the buffer is 32bit floats
         const normalize = false; // don't normalize
         const stride = 0; // how many bytes to get from one set of values to the next
         // 0 = use type and numComponents above
         const offset = 0; // how many bytes inside the buffer to start from
-        this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, buffers.position);
+        this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, this.#buffers.position);
         this.#gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexPosition,
+            this.#programInfo.attribLocations.vertexPosition,
             numComponents,
             type,
             normalize,
             stride,
             offset,
         );
-        this.#gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+        this.#gl.enableVertexAttribArray(this.#programInfo.attribLocations.vertexPosition);
     }
 
     initColorBuffer() {
@@ -158,21 +168,30 @@ class WebGL {
         return colorBuffer;
     }
 
-    setColorAttribute(buffers, programInfo) {
+    setColorAttribute() {
       const numComponents = 4;
       const type = this.#gl.FLOAT;
       const normalize = false;
       const stride = 0;
       const offset = 0;
-      this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, buffers.color);
+      this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, this.#buffers.color);
       this.#gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexColor,
+        this.#programInfo.attribLocations.vertexColor,
         numComponents,
         type,
         normalize,
         stride,
         offset
       );
-      this.#gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
+      this.#gl.enableVertexAttribArray(this.#programInfo.attribLocations.vertexColor);
+    }
+
+    render(now) {
+        now *= 0.001; // convert to seconds
+        this.#dt = now - this.#time;
+        this.time = now;
+        this.drawScene();
+        this.#rotation += this.#dt;
+      requestAnimationFrame(this.render.bind(this));
     }
 }
