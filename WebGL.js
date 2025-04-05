df37976fc788bd6eba1cb45169c0b97a264792a8
Athanasios Papapetrou <athanasios.papapetrou@netcompany.com>
2025-04-05 17:55:10 +0300

    1

---
+++ WebGL.js
@@ -0,0 +1,7 @@
+class WebGL {
+    static main() {
+        const gl = document.querySelector('canvas#gl').getContext('webgl');
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    }
+}
