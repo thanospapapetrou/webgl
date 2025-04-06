e12b9066aebb3f598fa5e3d43104adb64aeb0d5e
Athanasios Papapetrou <athanasios.papapetrou@netcompany.com>
2025-04-07 00:46:26 +0300

    Cleanup

--- js/WebGL.js
+++ js/WebGL.js
@@ -41,8 +41,14 @@ class WebGL {
                     modelViewMatrix: this.#gl.getUniformLocation(program, 'uModelViewMatrix')
                 }
             };
-            this.#buffers = this.initBuffers();
-            requestAnimationFrame(this.render.bind(this));
+            this.#load('./models/cube.json').then((response) => response.json()).then((cube) => {
+                this.#buffers = {
+                    positions: this.#createBuffer(this.#gl.ARRAY_BUFFER, new Float32Array(cube.positions)),
+                    colors: this.#createBuffer(this.#gl.ARRAY_BUFFER, new Float32Array(cube.colors)),
+                    indices: this.#createBuffer(this.#gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cube.indices))
+                };
+                requestAnimationFrame(this.render.bind(this));
+            });
         });
     }
 
@@ -63,7 +69,7 @@ class WebGL {
     }
 
     #compile(url, type) {
-        return this.#load(url).then((source) => {
+        return this.#load(url).then((response) => response.text()).then((source) => {
             const shader = this.#gl.createShader(type);
             this.#gl.shaderSource(shader, source);
             this.#gl.compileShader(shader);
@@ -75,50 +81,24 @@ class WebGL {
         });
     }
 
+    #createBuffer(type, data) {
+        const buffer = this.#gl.createBuffer();
+        this.#gl.bindBuffer(type, buffer);
+        this.#gl.bufferData(type, data, this.#gl.STATIC_DRAW);
+        return buffer;
+    }
+
     #load(url) {
         return fetch(url).then((response) => {
             if (!response.ok) {
                 throw new Error(WebGL.ERROR_LOADING(url, response.status));
             }
-            return response.text();
+            return response;
         });
     }
 
     initBuffers() {
-        const positionBuffer = this.initPositionBuffer();
-        const colorBuffer = this.initColorBuffer();
-        const indexBuffer = this.initIndexBuffer();
-        return {
-            position: positionBuffer,
-            color: colorBuffer,
-            indices: indexBuffer
-        };
-    }
 
-    initPositionBuffer() {
-      const positionBuffer = this.#gl.createBuffer();
-      this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, positionBuffer);
-      const positions = [
-        // Front face
-        -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
-
-        // Back face
-        -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
-
-        // Top face
-        -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
-
-        // Bottom face
-        -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
-
-        // Right face
-        1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
-
-        // Left face
-        -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
-      ];
-      this.#gl.bufferData(this.#gl.ARRAY_BUFFER, new Float32Array(positions), this.#gl.STATIC_DRAW);
-      return positionBuffer;
     }
 
     drawScene() {
@@ -146,34 +126,13 @@ class WebGL {
     }
 
     setPositionAttribute() {
-        this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, this.#buffers.position);
+        this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, this.#buffers.positions);
         this.#gl.vertexAttribPointer(this.#programInfo.attribLocations.vertexPosition, 3, this.#gl.FLOAT, false, 0, 0);
         this.#gl.enableVertexAttribArray(this.#programInfo.attribLocations.vertexPosition);
     }
 
-    initColorBuffer() {
-        const faceColors = [
-          [1.0, 1.0, 1.0, 1.0], // Front face: white
-          [1.0, 0.0, 0.0, 1.0], // Back face: red
-          [0.0, 1.0, 0.0, 1.0], // Top face: green
-          [0.0, 0.0, 1.0, 1.0], // Bottom face: blue
-          [1.0, 1.0, 0.0, 1.0], // Right face: yellow
-          [1.0, 0.0, 1.0, 1.0], // Left face: purple
-        ];
-        let colors = [];
-        for (let j = 0; j < faceColors.length; ++j) {
-          const c = faceColors[j];
-          // Repeat each color four times for the four vertices of the face
-          colors = colors.concat(c, c, c, c);
-        }
-        const colorBuffer = this.#gl.createBuffer();
-        this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, colorBuffer);
-        this.#gl.bufferData(this.#gl.ARRAY_BUFFER, new Float32Array(colors), this.#gl.STATIC_DRAW);
-        return colorBuffer;
-    }
-
     setColorAttribute() {
-      this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, this.#buffers.color);
+      this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, this.#buffers.colors);
       this.#gl.vertexAttribPointer(this.#programInfo.attribLocations.vertexColor, 4, this.#gl.FLOAT, false, 0, 0);
       this.#gl.enableVertexAttribArray(this.#programInfo.attribLocations.vertexColor);
     }
@@ -184,20 +143,5 @@ class WebGL {
         this.#rotation += WebGL.#VR * dt / WebGL.#MS_PER_S;
         this.drawScene();
         requestAnimationFrame(this.render.bind(this));
-    }
-
-    initIndexBuffer() {
-      const indexBuffer = this.#gl.createBuffer();
-      this.#gl.bindBuffer(this.#gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-      const indices = [
-         0,  1,  2,      0,  2,  3,    // front
-         4,  5,  6,      4,  6,  7,    // back
-         8,  9,  10,     8,  10, 11,   // top
-         12, 13, 14,     12, 14, 15,   // bottom
-         16, 17, 18,     16, 18, 19,   // right
-         20, 21, 22,     20, 22, 23,   // left
-      ];
-      this.#gl.bufferData(this.#gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), this.#gl.STATIC_DRAW);
-      return indexBuffer;
     }
 }
